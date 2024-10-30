67f82cde4c7e822ee1a830dd94fadc3bd91f1b5b
srikanth-sfu <srikanth.srikanth18@gmail.com>
2024-10-30 11:46:17 -0400

    add install scripts

---
+++ init_env.sh
@@ -0,0 +1,8 @@
+conda create -n nanodet python=3.8 -y
+conda activate nanodet
+pip install Cython matplotlib numpy onnx onnx-simplifier pyyaml omegaconf>=2.0.1 imagesize pycocotools tabulate tensorboard termcolor --no-index
+conda install pytorch torchvision cudatoolkit=11.1 -c pytorch pytorch-lightning=1.10.0 -c conda-forge 
+conda install torchmetrics tqdm
+cd $SLURM_TMPDIR
+git clone git@github.com:srikanth-sfu/nanodet.git
+# opencv-python
