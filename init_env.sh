e80a8f9dbc208711433fa2f001721fa2f04e519c
srikanth-sfu <srikanth.srikanth18@gmail.com>
2024-10-30 14:05:32 -0400

    change pt installer

--- init_env.sh
+++ init_env.sh
@@ -5,7 +5,7 @@ pip install Cython matplotlib numpy onnx onnx-simplifier pyyaml imagesize pycoco
 pip install --upgrade pip==23.0.1
 unset PIP_CACHE_DIR
 pip install omegaconf==2.0.2
-conda install pytorch torchvision cudatoolkit=11.1 pytorch-lightning=1.9.5 -c conda-forge -y 
+conda install pytorch=1.10.0 torchvision cudatoolkit=11.1 pytorch-lightning -c conda-forge -y 
 conda install torchmetrics tqdm -y
 python setup.py develop
 mkdir ckpt && cd ckpt
