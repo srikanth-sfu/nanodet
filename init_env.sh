140fbf0c1a03b1cf0ea1bb747072b228be7b4256
Srikanth Muralidharan <smuralid@cdr117.int.cedar.computecanada.ca>
2024-10-30 10:26:10 -0700

    add ckpt url

--- init_env.sh
+++ init_env.sh
@@ -7,3 +7,10 @@ unset PIP_CACHE_DIR
 pip install omegaconf==2.0.2
 conda install pytorch torchvision cudatoolkit=11.1 -c pytorch pytorch-lightning=1.9.5 -c conda-forge -y 
 conda install torchmetrics tqdm -y
+python setup.py develop
+mkdir ckpt && cd ckpt
+# nanodet_m.ckpt
+#wget https://drive.google.com/file/d/1ZkYucuLusJrCb_i63Lid0kYyyLvEiGN3/view?usp=sharing
+# nanodet-plus-m_320.pth
+#wget https://drive.google.com/file/d/1Dq0cTIdJDUhQxJe45z6rWncbZmOyh1Tv/view?usp=sharing
+#wget https://drive.google.com/file/d/1YvuEhahlgqxIhJu7bsL-fhaqubKcCWQc/view
