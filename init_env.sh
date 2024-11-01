48b5a5bf05d3b8a772f5a080d87e026b73e8a6c5
srikanth-sfu <srikanth.srikanth18@gmail.com>
2024-11-01 08:03:44 -0400

    change env install

--- init_env.sh
+++ init_env.sh
@@ -1,12 +1,12 @@
 conda create -n nanodet python=3.8 -y
 conda activate nanodet
 conda install kiwisolver opencv -y
-conda install tensorflow -y
-pip install Cython matplotlib numpy onnx ipdb onnx2tf onnxruntime onnx-simplifier pyyaml imagesize pycocotools tabulate tensorboard termcolor --no-index
+#pip install onnx2tf onnx_graphsurgeon
+pip install Cython matplotlib numpy onnx ipdb onnxruntime onnx-simplifier pyyaml imagesize pycocotools tabulate tensorboard termcolor --no-index
 pip install --upgrade pip==23.0.1
 unset PIP_CACHE_DIR
 pip install omegaconf==2.0.2
-conda install pytorch=1.11.0 torchvision cudatoolkit=11.1 -c conda-forge -y 
+conda install pytorch=1.13.1 torchvision cudatoolkit=11.1 tensorflow=1.11.0 -c conda-forge -y 
 conda install pytorch-lightning=1.9.5 torchmetrics tqdm -y
 python setup.py develop
 mkdir ckpt && cd ckpt
